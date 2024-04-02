adf81c644835e07685840bb3ce5fcca319d66ff3
walisson150 <130377955+walisson150@users.noreply.github.com>
2024-04-02 11:17:42 -0300

    poooo

--- main.js
+++ main.js
@@ -15,8 +15,9 @@ for(let i=0;i <botoes.length;i++){
     }
 }
 
-const contadores = document.querySelectorAll(".contador");
 
+const contadores = document.querySelectorAll(".contador");
+ 
 const tempoObjetivo1 = new Date("2024-10-05"); 
 
 contadores[0].textContent = tempo0bjetivo1; 
