e7e3663787078903a5e7832824c60f2b2ab514f7
walisson150 <130377955+walisson150@users.noreply.github.com>
2024-04-02 11:12:25 -0300

    onde

--- main.js
+++ main.js
@@ -17,5 +17,9 @@ for(let i=0;i <botoes.length;i++){
 
 const contadores = document.querySelectorAll(".contador");
 
+const tempoObjetivo1 = new Date("2024-10-05"); 
+
+contadores[0].textContent = tempo0bjetivo1; 
+
 contadores[0].textContent = "Contagem regressiva";
 
