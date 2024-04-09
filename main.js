5dc89d686905984a9556f61547ad492a76eeae56
walisson150 <130377955+walisson150@users.noreply.github.com>
2024-04-09 10:53:13 -0300

    ewewew

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const botoes = document.querySelectorAll(".botao");
 
 for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick
+    botoes[i].onclick = function(){
+ botoes[i].classList.add("ativo");
+    }
     console.log();
 }
